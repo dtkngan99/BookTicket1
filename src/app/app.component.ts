b33daef060a61c05cf8a4282db6cdb83fde2f013
dtkng <86214780+dtkngan99@users.noreply.github.com>
2021-09-18 11:33:10 +0700

    book ticket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { ListTicket, Ticket } from './../ticket';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BookTicket';
+  tickets=ListTicket;
+  seatSelected: Ticket;
+
+
+
+//----------------------------------------------------------------
+  
+
+  onChangeStatus(ticket, status):void {
+    this.seatSelected=ticket;
+    console.log(this.seatSelected);
+    this.tickets[ticket.soghe-1].trangthai=status;
+  }
+
+  listTicketBooked(){
+    let list=this.tickets.filter(i=>i.trangthai==1);
+    console.log('11111');
+    console.log(this.tickets);
+    return list;
+  }
+
+  checkChangeStatus(ticket, status) {
+    if(ticket.trangthai==status) return true;
+    return false;
+  }
 }
---
+++ src/ticket.ts
@@ -0,0 +1,19 @@
+export class Ticket {
+    soghe: number;
+    gia: number;
+    trangthai: number;
+};
+
+export const ListTicket:Array<Ticket>=
+[
+    {soghe: 1, gia: 100, trangthai:0},
+    {soghe: 2, gia: 100, trangthai:0},
+    {soghe: 3, gia: 100, trangthai:0},
+    {soghe: 4, gia: 100, trangthai:0},
+    {soghe: 5, gia: 100, trangthai:0},
+    {soghe: 6, gia: 100, trangthai:0},
+    {soghe: 7, gia: 100, trangthai:0},
+    {soghe: 8, gia: 100, trangthai:0},
+    {soghe: 9, gia: 100, trangthai:0},
+    {soghe: 10, gia: 100, trangthai:0}
+]
